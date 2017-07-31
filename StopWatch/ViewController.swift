e84d4808c59d6698bab261a245c1d2c8525823e5
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-31 22:13:00 +0800

    Improved Controls and Buttons :D

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -15,10 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     
-    var time = 0
+    var time : Double = 0.00
     var timer = Timer()
+    var isPlaying = false
    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,23 +26,35 @@ class ViewController: UIViewController {
 
     @IBAction func playButtonTouched(_ sender: Any) {
     
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.UpdateTimer), userInfo: nil, repeats: true)
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.UpdateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
     }
     
     
     @IBAction func pauseButtonTouched(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
         timer.invalidate()
+        isPlaying = false
+        
     }
     
     @IBAction func resetButtonTouched(_ sender: Any) {
-        time = 0
+        time = 0.00
+        timeLabel.text = String(time)
         timer.invalidate()
+        playButton.isEnabled = true
+        pauseButton.isEnabled = true
+        isPlaying = false
+        
     }
     
     
     func UpdateTimer() {
         time += 1
-        timeLabel.text = String(time)
+        timeLabel.text = String(format: "%.f", time)
     }
 
     
